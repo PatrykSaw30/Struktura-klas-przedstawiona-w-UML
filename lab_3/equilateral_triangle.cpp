6c0e7d8d080323cd20041b1e93d555e74b805eee
PatrykSaw30 <sawickiipatrykk@gmail.com>
2026-03-19 20:36:53 +0100

    Delete lab_3 directory

--- lab_3/equilateral_triangle.cpp
+++
@@ -1,7 +0,0 @@
-#include "equilateral_triangle.h"
-
-EquilateralTriangle::EquilateralTriangle(std::vector<Line> l, int h_val) : Triangle(h_val, l) {}
-EquilateralTriangle::EquilateralTriangle(std::vector<Line> l) : Triangle(l) {}
-
-float EquilateralTriangle::calcArea() { return Triangle::calcArea(); }
-float EquilateralTriangle::calcCircum() { return Triangle::calcCircum(); }
